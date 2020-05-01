a6d8530bc73c84c5fd8a1cd5d4e847d4efcabfc5
Abdelbaset65 <50206880+Abdelbaset65@users.noreply.github.com>
2020-05-01 18:32:17 +0200

    updated gdt.asm

--- sources/includes/second_stage/gdt.asm
+++ sources/includes/second_stage/gdt.asm
@@ -1,12 +1,30 @@
+; Initializing the Global Descriptor Table 
+; And Setting the needed attributes to the CODE and DATA segments
+
 GDT64:
     .Null: equ $ - GDT64         ; The null descriptor.
-    ; This function need to be written by you.
-
+    dw 0
+    dw 0
+    db 0
+    db 0
+    db 0
+    db 0
     .Code: equ $ - GDT64         ; The Kernel code descriptor.
-    ; This function need to be written by you.
-
+    dw 0
+    dw 0
+    db 0
+    db 10011000b
+    db 00100000b
+    db 0
     .Data: equ $ - GDT64         ; The Kernel data descriptor.
-    ; This function need to be written by you.
-
-    .Pointer:
-    ; This function need to be written by you.
+    dw 0
+    dw 0
+    db 0
+    db 10010011b
+    db 00000000b 
+    db 0
+ALIGN 4
+    dw 0
+.Pointer:
+    dw $ - GDT64 -1
+    dd GDT64
